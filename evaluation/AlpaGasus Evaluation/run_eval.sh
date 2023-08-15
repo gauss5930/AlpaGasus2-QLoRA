072b0d0e7c2a103023aea9e54a24aee526ea54ab
Hyunwoo Ko <80087878+gauss5930@users.noreply.github.com>
2023-08-15 16:27:23 +0900

    Update run_eval.sh

--- evaluation/AlpaGasus Evaluation/run_eval.sh
+++ evaluation/AlpaGasus Evaluation/run_eval.sh
@@ -1,5 +1,5 @@
 for eval_file in 'koala_seed_0.json' 'sinstruct_seed_0.json' 'wizardlm_seed_0.json' 'vicuna_seed_0.json'
 do
-    python eval.py -qa ./results/${eval_file} -k1 alpaca -k2 alpagasus --batch_size 10 --max_tokens 256
-    python eval.py -qa ./results/${eval_file} -k1 alpagasus -k2 alpaca --batch_size 10 --max_tokens 256
+    python eval.py -qa ./results/${eval_file} -k1 alpaca2 -k2 alpagasus2 --batch_size 10 --max_tokens 256 --output_dir ./rating_data/
+    python eval.py -qa ./results/${eval_file} -k1 alpagasus2 -k2 alpaca2 --batch_size 10 --max_tokens 256 --output_dir ./rating_data/
 done
