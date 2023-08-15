3a88f68561fcc76d5f5d28a6577dbc004ddf62b9
Hyunwoo Ko <80087878+gauss5930@users.noreply.github.com>
2023-08-15 16:40:03 +0900

    Update generate.py

--- evaluation/AlpaGasus Evaluation/response_data/generate.py
+++ evaluation/AlpaGasus Evaluation/response_data/generate.py
@@ -22,13 +22,15 @@ def main(
   model = PeftModel.from_pretrained(model, lora_weight)
 
   test_data = ['koala_test_set.jsonl', 'sinstruct_test_set.jsonl', 'vicuna_test_set.jsonl']
-  col = ['prompt', '', 'text']
+  col = ['prompt', 'instruction', 'text']
   
-  result = []
+
   for i in range(len(test_data)):
+    result = []
     path = test_path + test_data[i]
     count = 0
     name = test_data[i].split('_')[0]
+    sv_path = save_path + name + "_seed_0.json"
     with jsonlines.open(path) as f:
       for line in f:
         if col[i]:
@@ -44,11 +46,20 @@ def main(
         count += 1
         index = name + '_' + str(count)
         ind = {"question_id": index}
-        output = {"model_output": model_output}
+        instruction = {col[i]: line[col[i]]}
+        ind.update(instruction)
+        if col[i] == 'instruction':
+          if line['instances'][0]['input']:
+            input = {'instances': [{'input': line['instances'][0]['input']}]}
+            ind.update(input)
+          else:
+            input = {'instances': [{'input': ""}]}
+            ind.update(input)
+        output = {"alpagasus2": model_output}    # Change the name of model to what model you use
         result.append(ind.update(output))
   
-  with open(save_path, "x") as json_file:
-    json.dump(result, json_file, indent=4)
+    with open(save_path, "x") as json_file:
+      json.dump(result, json_file, indent=4)
 
 if __name__ == "__main__":
   fire.Fire(main)
