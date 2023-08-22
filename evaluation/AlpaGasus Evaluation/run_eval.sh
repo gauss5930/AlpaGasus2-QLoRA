b6a95cc4b80224e01feca4feefb73ffd9d14e5b4
Hyunwoo Ko <80087878+gauss5930@users.noreply.github.com>
2023-08-22 10:07:32 +0900

    Update run_eval.sh

--- evaluation/AlpaGasus Evaluation/run_eval.sh
+++ evaluation/AlpaGasus Evaluation/run_eval.sh
@@ -1,4 +1,4 @@
-export API_KEY='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'
+export API_KEY='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'
 for eval_file in 'koala_seed_0.json', 'sinstruct_seed_0.json', 'vicuna_seed_0.json'
 do
     python eval.py -qa ./response_data/results/${eval_file} -k1 alpaca2 -k2 alpagasus2 --batch_size 10 --max_tokens 256 --output_dir ./rating_data/
