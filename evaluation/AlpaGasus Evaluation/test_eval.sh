33aed963220e1975358b1304d03cc74a5e4275fc
Hyunwoo Ko <80087878+gauss5930@users.noreply.github.com>
2023-08-23 10:36:54 +0900

    Delete test_eval.sh

--- evaluation/AlpaGasus Evaluation/test_eval.sh
+++
@@ -1,4 +0,0 @@
-for eval_file in 'koala_seed_0.json'
-do
-    python test_eval.py -qa ./response_data/results/${eval_file} -k1 alpaca2 -k2 alpagasus2 --batch_size 10 --max_tokens 256 --output_dir ./rating_data/
-done
