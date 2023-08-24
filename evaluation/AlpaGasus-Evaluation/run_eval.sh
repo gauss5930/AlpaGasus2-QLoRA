048f02597691a2785a5e545f9f99d90ee9a0bab7
Hyunwoo Ko <80087878+gauss5930@users.noreply.github.com>
2023-08-24 10:44:03 +0900

    Delete run_eval.sh

--- evaluation/AlpaGasus-Evaluation/run_eval.sh
+++
@@ -1,5 +0,0 @@
-for eval_file in 'koala_seed_0.json', 'sinstruct_seed_0.json', 'vicuna_seed_0.json'
-do
-    python evaluation.py -qa /content/AlpaGasus2-QLoRA/evaluation/AlpaGasus-Evaluation/response_data/results/${eval_file} -k1 alpaca2 -k2 alpagasus2 --batch_size 10 --max_tokens 256 --output_dir /content/AlpaGasus2-QLoRA/evaluation/AlpaGasus-Evaluation/rating_data/
-    python evaluation.py -qa /content/AlpaGasus2-QLoRA/evaluation/AlpaGasus-Evaluation/response_data/results/${eval_file} -k1 alpagasus2 -k2 alpaca2 --batch_size 10 --max_tokens 256 --output_dir /content/AlpaGasus2-QLoRA/evaluation/AlpaGasus-Evaluation/rating_data/
-done
